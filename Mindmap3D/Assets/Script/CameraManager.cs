23954f8c51d5528469ba48df1d051e3d1090fb03
HW21A018 <hw21a018@outlook.jp>
2024-08-23 13:28:13 +0900

    Update: カメラの移動・回転機能を実装

--- Mindmap3D/Assets/Script/CameraManager.cs
+++ Mindmap3D/Assets/Script/CameraManager.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float moveSpeed = 10f; 
+    public float lookSpeed = 2f;
 
-    // Update is called once per frame
+    private float rotationX = 0f;
+    private float rotationY = 0f;
+
     void Update()
     {
-        
+        // 移動関係
+        float moveX = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float moveZ = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Translate(moveX, 0, moveZ);
+
+        // 回転関係
+        if (Input.GetMouseButton(1)) // 右クリックが押されている間
+        {
+            rotationX += Input.GetAxis("Mouse X") * lookSpeed;
+            rotationY -= Input.GetAxis("Mouse Y") * lookSpeed;
+            rotationY = Mathf.Clamp(rotationY, -90f, 90f); // カメラの上下回転を制限
+            transform.localEulerAngles = new Vector3(rotationY, rotationX, 0);
+        }
     }
 }
