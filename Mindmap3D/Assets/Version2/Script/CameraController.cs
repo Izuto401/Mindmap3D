6bb22195ff5e472359f2df15083f72ac35ae31d6
Izuto401 <iwamizukaito@gmail.com>
2024-12-25 14:35:26 +0900

    WASDでのカメラ移動機能

--- Mindmap3D/Assets/Version2/Script/CameraController.cs
+++ Mindmap3D/Assets/Version2/Script/CameraController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CameraController : MonoBehaviour
 {
@@ -10,11 +11,17 @@ public class CameraController : MonoBehaviour
     // カメラのズーム速度
     public float zoomSpeed = 50f;
 
+    // カメラの移動速度
+    public float moveSpeed = 10f;
+
     // ノードマネージャーの参照
     private NodeManager nodeManager;
 
     // 選択されたノードの参照
     private Transform selectedNode;
+
+    // ノード名入力フィールドの参照
+    public TMP_InputField nodeNameInputField;
 
     void Start()
     {
@@ -24,6 +31,12 @@ public class CameraController : MonoBehaviour
 
     void Update()
     {
+        // 入力フィールドがフォーカスされている場合はカメラ操作を無視
+        if (nodeNameInputField != null && nodeNameInputField.isFocused)
+        {
+            return;
+        }
+
         // 選択されたノードの更新
         if (nodeManager.SelectedNodes.Count > 0)
         {
@@ -33,6 +46,11 @@ public class CameraController : MonoBehaviour
         {
             selectedNode = null;
         }
+
+        // WASDキーでカメラを移動
+        float horizontal = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime; // A, Dキーで横移動
+        float vertical = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;     // W, Sキーで縦移動
+        transform.Translate(horizontal, 0, vertical);
 
         // ノードを選択せずドラッグでカメラの角度変更
         if (Input.GetMouseButton(1) && selectedNode == null)
